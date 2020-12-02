1119cde6b3ee33c2a1a9d7cc8c87bbfcc564f07c
acw <alex_acw@outlook.com>
2020-12-02 20:24:42 +0800

    [SpringBoot][Alex Au] feat: implement getAll for MemoryEmployeeRepository

--- src/main/java/com/thoughtworks/springbootemployee/repositories/MemoryEmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repositories/MemoryEmployeeRepository.java
@@ -3,15 +3,19 @@ package com.thoughtworks.springbootemployee.repositories;
 import com.thoughtworks.springbootemployee.Employee;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class MemoryEmployeeRepository implements EmployeeRepository {
+    private List<Employee> employees = new ArrayList<>();
+
     public List<Employee> getAll() {
-        return null;
+        return this.employees;
     }
 
-    public Employee add(Employee capture) {
-        return null;
+    public Employee add(Employee employee) {
+        employees.add(employee);
+        return employee;
     }
 }
