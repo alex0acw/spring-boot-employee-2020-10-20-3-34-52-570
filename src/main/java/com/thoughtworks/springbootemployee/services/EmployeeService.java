d1875b1f4b8168afa6b98d238e2b668931784453
acw <alex_acw@outlook.com>
2020-12-02 18:52:33 +0800

    [SpringBoot][Alex Au] test, fix test should_return_all_employees_when_get_all_given_employees

--- src/main/java/com/thoughtworks/springbootemployee/services/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/services/EmployeeService.java
@@ -2,15 +2,18 @@ package com.thoughtworks.springbootemployee.services;
 
 import com.thoughtworks.springbootemployee.Employee;
 import com.thoughtworks.springbootemployee.repositories.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EmployeeService {
-    @Autowired
-    private EmployeeRepository employeeRepository;
+    private final  EmployeeRepository employeeRepository ;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
 
     public List<Employee> getAll() {
         return null;
--- src/test/java/com/thoughtworks/springbootemployee/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeeServiceTest.java
@@ -4,27 +4,34 @@ import com.thoughtworks.springbootemployee.repositories.EmployeeRepository;
 import com.thoughtworks.springbootemployee.services.EmployeeService;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+@SpringBootTest
 public class EmployeeServiceTest {
 
+
     @Test
-    public void should_employee_service_return_all_employees_when_get_all_given_employees() {
+    public void should_return_all_employees_when_get_all_given_employees() {
         //given
-        EmployeeService employeeService = new EmployeeService();
         EmployeeRepository employeeRepository = Mockito.mock(EmployeeRepository.class);
-        List<Employee> employeeList = new ArrayList<>();
-        employeeList.add(new Employee());
-        Mockito.when(employeeRepository.getAll()).thenReturn(employeeList);
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
+        final List<Employee> expected = Arrays.asList(
+                new Employee(1, "foo", 18, "Male", 100),
+                new Employee(1, "bar", 20, "Female", 120));
+
+        when(employeeRepository.getAll()).thenReturn(expected);
 
         //when
         List<Employee> actualEmployees = employeeService.getAll();
 
         //then
-        assertEquals(employeeList,actualEmployees);
+        assertEquals(expected, actualEmployees);
     }
 }
