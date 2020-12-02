1f6579a5345a8cc5b3af04dfe27dfe954619be9c
acw <alex_acw@outlook.com>
2020-12-02 21:20:34 +0800

    [SpringBoot][Alex Au] test: should_update_company_with_same_id_when_add_given_companies_and_modified_company

--- src/test/java/com/thoughtworks/springbootemployee/MemoryCompanyRepositoryTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/MemoryCompanyRepositoryTest.java
@@ -73,42 +73,29 @@ public class MemoryCompanyRepositoryTest {
         //given
         assertEquals(expected, actual);
     }
-//
-//    @Test
-//    public void should_return_males_result_when_find_by_gender_given_added_employees() {
-//        //should
-//        EmployeeRepository employeeRepository = new MemoryEmployeeRepository();
-//        List<Employee> expected = Arrays.asList(
-//                new Employee(1, "foo", 18, "Male", 100),
-//                new Employee(2, "bar", 20, "Male", 120));
-//        for (Employee employee : expected) {
-//            employeeRepository.add(employee);
-//        }
-//        employeeRepository.add(new Employee(3, "bar", 20, "Female", 120));
-//        employeeRepository.add(new Employee(4, "bar", 20, "Female", 120));
-//        //when
-//        List<Employee> actual = employeeRepository.findByGender("Male");
-//        //given
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void should_update_employee_with_same_id_when_add_given_employees_and_modified_employee() {
-//        //should
-//        EmployeeRepository employeeRepository = new MemoryEmployeeRepository();
-//
-//        for (Employee employee : Arrays.asList(
-//                new Employee(1, "foo", 18, "Male", 100),
-//                new Employee(2, "bar", 20, "Male", 120))) {
-//            employeeRepository.add(employee);
-//        }
-//        Employee expected = new Employee(1, "foooo", 22, "Male", 99999);
-//        //when
-//        employeeRepository.add(expected);
-//        Employee actual = employeeRepository.findById(1);
-//        //given
-//        assertEquals(expected, actual);
-//    }
+
+
+    @Test
+    public void should_update_company_with_same_id_when_add_given_companies_and_modified_company() {
+        //should
+        CompanyRepository companyRepository = new MemoryCompanyRepository();
+
+        for (Company company : Arrays.asList(
+                new Company(1, "foo", new ArrayList<>()),
+                new Company(2, "bar", new ArrayList<>()))) {
+            companyRepository.add(company);
+        }
+        Company expected = new Company(1, "foo",
+                Arrays.asList(
+                        new Employee(1, "foo", 18, "Male", 100),
+                        new Employee(2, "bar", 20, "Male", 120))
+        );
+        //when
+        companyRepository.add(expected);
+        Company actual = companyRepository.findById(1);
+        //given
+        assertEquals(expected, actual);
+    }
 //
 //    @Test
 //    public void should_delete_employee_when_delete_given_employees_and_id() {
