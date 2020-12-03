bfd6d0ce5f85f46aec6d0c7221e888d3d5ed48eb
Alex Au <alex_acw@outlook.com>
2020-12-03 19:42:02 +0800

    [SpringBoot][Alex Au] test: fix CompanyIntegrationTest should_create_and_return_company_when_create_given_company

--- src/test/java/com/thoughtworks/springbootemployee/integration/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integration/CompanyIntegrationTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -18,6 +19,7 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -125,59 +127,40 @@ public class CompanyIntegrationTest {
                 .andExpect(jsonPath("$.content[1].employees[0].salary").value(120))
                 .andExpect(jsonPath("$.content", hasSize(2)));
     }
-//
-//
-//    @Test
-//    public void should_return_one_gender_companies_when_get_all_companies_with_gender() throws Exception {
-//
-//        //given
-//        List<Company> companyList = Arrays.asList(
-//                new Company("foo", 20, "Male", 120),
-//                new Company("bar", 20, "Female", 120),
-//                new Company("b", 20, "Male", 120),
-//                new Company("a", 20, "Female", 120),
-//                new Company("c", 20, "Male", 120)
-//        );
-//        companyList.forEach(company -> companyRepository.save(company));
-//        //when
-//        //then
-//        mockMvc.perform(get("/companies/?gender=Female"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.[0].id").isString())
-//                .andExpect(jsonPath("$.[0].name").value("bar"))
-//                .andExpect(jsonPath("$.[0].age").value(20))
-//                .andExpect(jsonPath("$.[0].gender").value("Female"))
-//                .andExpect(jsonPath("$.[0].salary").value(120))
-//                .andExpect(jsonPath("$.[1].id").isString())
-//                .andExpect(jsonPath("$.[1].name").value("a"))
-//                .andExpect(jsonPath("$.[1].age").value(20))
-//                .andExpect(jsonPath("$.[1].gender").value("Female"))
-//                .andExpect(jsonPath("$.[1].salary").value(120));
-//    }
-//
-//    @Test
-//    public void should_return_company_when_create_given_company() throws Exception {
-//        //given
-//        String companyAsJson = "{\n" +
-//                "        \"name\": \"foobar\",\n" +
-//                "        \"age\": 3,\n" +
-//                "        \"gender\": null,\n" +
-//                "        \"salary\": 4\n" +
-//                "    }";
-//        //when
-//        //then
-//        mockMvc
-//                .perform(post("/companies")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(companyAsJson)
-//                )
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").isString())
-//                .andExpect(jsonPath("$.name").value("foobar"))
-//                .andExpect(jsonPath("$.age").value(3))
-//                .andExpect(jsonPath("$.salary").value(4));
-//
-//    }
+
+
+    @Test
+    public void should_create_and_return_company_when_create_given_company() throws Exception {
+        //given
+        Employee employee = new Employee("bar", 20, "Female", 120);
+        employee = employeeRepository.save(employee);
+        String companyAsJson = String.format(
+                "{\n" +
+                        "    \"companyName\": \"a\",\n" +
+                        "    \"employees\": [\n" +
+                        "        {\n" +
+                        "            \"id\": \"%s\"\n" +
+                        "        }\n" +
+                        "    ]\n" +
+                        "}",
+                employee.getId());
+        //when
+        //then
+        mockMvc
+                .perform(post("/companies")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(companyAsJson)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isString())
+                .andExpect(jsonPath("$.companyName").value("a"))
+                .andExpect(jsonPath("$.employees[0].id").isString())
+                .andExpect(jsonPath("$.employees[0].name").value("bar"))
+                .andExpect(jsonPath("$.employees[0].age").value(20))
+                .andExpect(jsonPath("$.employees[0].gender").value("Female"))
+                .andExpect(jsonPath("$.employees[0].salary").value(120));
+
+    }
 //
 //    @Test
 //    public void should_return_updated_company_when_update_company_given_an_company() throws Exception {
