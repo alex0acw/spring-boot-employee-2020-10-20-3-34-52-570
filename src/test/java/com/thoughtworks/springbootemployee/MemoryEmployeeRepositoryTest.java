1b98c1751b730860be521fc95562cfe214dff53b
acw <alex_acw@outlook.com>
2020-12-03 08:55:25 +0800

    [SpringBoot][Alex Au] test: should_add_employee_throws_exception_when_add_given_duplicate_employees

--- src/test/java/com/thoughtworks/springbootemployee/MemoryEmployeeRepositoryTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/MemoryEmployeeRepositoryTest.java
@@ -7,23 +7,24 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static com.thoughtworks.springbootemployee.repositories.MemoryEmployeeRepository.DUPLICATE_EMPLOYEE_ID_MESSAGE;
+import static com.thoughtworks.springbootemployee.repositories.MemoryEmployeeRepository.NO_SUCH_EMPLOYEE_MESSAGE;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MemoryEmployeeRepositoryTest {
     @Test
     public void should_return_all_employees_when_get_all_given_added_employees() {
-        //should
+        //given
         EmployeeRepository employeeRepository = new MemoryEmployeeRepository();
         List<Employee> expected = Arrays.asList(
                 new Employee(1, "foo", 18, "Male", 100),
-                new Employee(1, "bar", 20, "Female", 120));
+                new Employee(2, "bar", 20, "Female", 120));
         for (Employee employee : expected) {
             employeeRepository.add(employee);
         }
         //when
         List<Employee> actual = employeeRepository.findAll();
-        //given
+        //then
         assertEquals(expected, actual);
     }
 
@@ -36,24 +37,24 @@ public class MemoryEmployeeRepositoryTest {
         employeeRepository.add(new Employee(2, "bar", 20, "Female", 120));
         //when
         Employee actual = employeeRepository.findById(1);
-        //given
+        //then
         assertEquals(expected, actual);
     }
 
     @Test
     public void should_return_null_when_get_by_id_given_added_employees_and_invalid_id() {
-        //should
+        //given
         EmployeeRepository employeeRepository = new MemoryEmployeeRepository();
         employeeRepository.add(new Employee(2, "bar", 20, "Female", 120));
         //when
         Employee actual = employeeRepository.findById(1);
-        //given
+        //then
         assertNull(actual);
     }
 
     @Test
     public void should_return_paged_result_given_added_employees() {
-        //should
+        //given
         EmployeeRepository employeeRepository = new MemoryEmployeeRepository();
         List<Employee> expected = Arrays.asList(
                 new Employee(1, "foo", 18, "Male", 100),
@@ -65,13 +66,13 @@ public class MemoryEmployeeRepositoryTest {
         employeeRepository.add(new Employee(4, "bar", 20, "Female", 120));
         //when
         List<Employee> actual = employeeRepository.findAllPaged(0, 2);
-        //given
+        //then
         assertEquals(expected, actual);
     }
 
     @Test
     public void should_return_males_result_when_find_by_gender_given_added_employees() {
-        //should
+        //given
         EmployeeRepository employeeRepository = new MemoryEmployeeRepository();
         List<Employee> expected = Arrays.asList(
                 new Employee(1, "foo", 18, "Male", 100),
@@ -83,13 +84,13 @@ public class MemoryEmployeeRepositoryTest {
         employeeRepository.add(new Employee(4, "bar", 20, "Female", 120));
         //when
         List<Employee> actual = employeeRepository.findByGender("Male");
-        //given
+        //then
         assertEquals(expected, actual);
     }
 
     @Test
     public void should_update_employee_with_same_id_when_add_given_employees_and_modified_employee() {
-        //should
+        //given
         EmployeeRepository employeeRepository = new MemoryEmployeeRepository();
 
         for (Employee employee : Arrays.asList(
@@ -99,15 +100,15 @@ public class MemoryEmployeeRepositoryTest {
         }
         Employee expected = new Employee(1, "foooo", 22, "Male", 99999);
         //when
-        employeeRepository.add(expected);
+        employeeRepository.update(expected);
         Employee actual = employeeRepository.findById(1);
-        //given
+        //then
         assertEquals(expected, actual);
     }
 
     @Test
     public void should_delete_employee_when_delete_given_employees_and_id() {
-        //should
+        //given
         EmployeeRepository employeeRepository = new MemoryEmployeeRepository();
 
         for (Employee employee : Arrays.asList(
@@ -118,7 +119,18 @@ public class MemoryEmployeeRepositoryTest {
         //when
         employeeRepository.deleteById(1);
         Employee actual = employeeRepository.findById(1);
-        //given
+        //then
         assertNull(actual);
+    }
+
+    @Test
+    public void should_add_employee_throws_exception_when_add_given_duplicate_employees() {
+        //given
+        EmployeeRepository employeeRepository = new MemoryEmployeeRepository();
+        employeeRepository.add(new Employee(1, "foo", 18, "Male", 100));
+        //when
+        //then
+        IllegalArgumentException actual = assertThrows(IllegalArgumentException.class, () -> employeeRepository.add(new Employee(1, "foo", 18, "Male", 100)));
+        assertEquals(DUPLICATE_EMPLOYEE_ID_MESSAGE, actual.getMessage());
     }
 }
