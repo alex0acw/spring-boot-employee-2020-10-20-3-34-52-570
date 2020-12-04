60a1b95f057f4c546f05053720e39ea4f16a721f
Dominic852 <43085764+Dominic852@users.noreply.github.com>
2020-12-04 14:16:19 +0800

    [SpringBoot][Dominic Leung and Alex Au] feat: add handler to handle NOT found exception

--- src/main/java/com/thoughtworks/springbootemployee/advice/GlobalControllerExceptionHandler.java
+++ src/main/java/com/thoughtworks/springbootemployee/advice/GlobalControllerExceptionHandler.java
@@ -10,12 +10,11 @@ import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
-//    @ResponseStatus(
-//            value = HttpStatus.NOT_FOUND,
-//            reason = "Resources not found")  // 409
-//    @ExceptionHandler(NoSuchElementException.class)
-//    public void notFound() {
-//    }
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public ErrorResponse handleNotFound(NoSuchElementException exception) {
+        return new ErrorResponse(exception.getMessage(),HttpStatus.NOT_FOUND.name());
+    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
--- src/main/java/com/thoughtworks/springbootemployee/services/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/services/CompanyService.java
@@ -48,12 +48,7 @@ public class CompanyService {
         Optional<Company> company = companyRepository.findById(id);
         if (company.isPresent()) {
             updatedCompany.setId(company.get().getId());
-            Optional<Company> savedCompany = companyRepository.findById(companyRepository.save(updatedCompany).getId());
-            if (savedCompany.isPresent())
-                return savedCompany.get();
-            else
-                throw new IllegalStateException("update company failed to fetch updated object.");
-
+            return  companyRepository.findById(companyRepository.save(updatedCompany).getId()).get();
         } else
             throw new NoSuchElementException();
     }
